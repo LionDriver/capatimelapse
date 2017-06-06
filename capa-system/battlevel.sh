b53d5f47bcfd5988568df5e15561a2b768ae88ec
sosteen <sosteen@gmail.com>
2017-06-06 11:11:44 -0700

    removed unused files, pointed to correct dir

--- capa-system/battlevel.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#
-
-/usr/local/bin/lifepo4wered-cli get vbat > /home/pi/workspace/batterylevel/batt.txt 2>&1
--- capa-system/cputemptext.sh
+++ capa-system/cputemptext.sh
@@ -4,4 +4,4 @@
 
 TEMPERATURE=`cat /sys/class/thermal/thermal_zone0/temp`
 TEMPERATURE=`echo -n ${TEMPERATURE:0:2}; echo -n .; echo -n ${TEMPERATURE:2}`
-echo $TEMPERATURE > /home/pi/workspace/cputemp/cputemp.txt 2>&1
+echo $TEMPERATURE > /home/pi/timelapse/cputemp/cputemp.txt 2>&1
