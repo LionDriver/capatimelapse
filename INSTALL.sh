dfbf81f246cd8b84066275531589cbe1579de986
sosteen <sosteen@gmail.com>
2017-06-13 11:43:52 -0700

    installer remove hard links

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# capatimelapse installer
+# capatimelapse INSTALL.sh
 # For use on raspbian with raspberry pi camera
-# Version 0.0.8 SlumberMachine 6/12/2017
+# Version 0.0.9 SlumberMachine 6/12/2017
 # TODO system items & database needs to be added
 # TODO Pipoint options and setup
 echo "This is the capatimelapse installer"
@@ -10,6 +10,10 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root or using sudo"
   exit
 fi
+
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+
 echo "Running updates and installing Dependencies"
 apt-get update && sudo apt-get -y dist-upgrade;
 apt-get -y install lighttpd php5-common php5-cgi php5 php5-mysql python-picamera python3-picamera python-mysqldb;
@@ -20,12 +24,12 @@ apt-get -y install mysql-server
 echo "Moving things into place"
 lighttpd-enable-mod fastcgi-php;
 rm /var/www/html/index.lighttpd.html
-cp -R /home/pi/capatimelapse/php /home/pi/capatimelapse/js /home/pi/capatimelapse/images /home/pi/capatimelapse/fonts /home/pi/capatimelapse/css /var/www/html/
-cp /home/pi/capatimelapse/gallery.php /home/pi/capatimelapse/index.html /home/pi/capatimelapse/system.php  /var/www/html/
-ln -s /home/pi/capatimelapse/pics /var/www/html/pics
-chown -R www-data:www-data /home/pi/capatimelapse/pics
-cp /home/pi/capatimelapse/capa-system/config.txt /var/www/html/
-ln -s /var/www/html/config.txt /home/pi/capatimelapse/config.txt
+cp -R ${SCRIPTPATH}/php ${SCRIPTPATH}/js ${SCRIPTPATH}/images ${SCRIPTPATH}/fonts ${SCRIPTPATH}/css /var/www/html/
+cp ${SCRIPTPATH}/gallery.php ${SCRIPTPATH}/index.html ${SCRIPTPATH}/system.php  /var/www/html/
+ln -s ${SCRIPTPATH}/pics /var/www/html/pics
+chown -R www-data:www-data ${SCRIPTPATH}/pics
+cp ${SCRIPTPATH}/capa-system/config.txt /var/www/html/
+ln -s /var/www/html/config.txt ${SCRIPTPATH}/config.txt
 
 echo '<?php $servername = "localhost"; $username = "monitor"; $password = "23rdqw"; $dbname = "images"; ?>' > db.php
 mv db.php /var/www/html/
@@ -36,7 +40,7 @@ mysql -u root -p23rdqw -e "CREATE DATABASE images";
 mysql -u root -p23rdqw -e "CREATE USER 'monitor'@'localhost' IDENTIFIED BY '23rdqw'";
 mysql -u root -p23rdqw -e "GRANT ALL PRIVILEGES ON images.* TO 'monitor'@'localhost'";
 mysql -u root -p23rdqw -e "FLUSH PRIVILEGES";
-mysql -u root -p23rdqw images < /home/pi/capatimelapse/capa-system/images.sql
+mysql -u root -p23rdqw images < ${SCRIPTPATH}/capa-system/images.sql
 
 echo 'www-data ALL=(ALL) NOPASSWD:/sbin/shutdown -h now' | sudo EDITOR='tee -a' visudo
 echo 'www-data ALL=(ALL) NOPASSWD:/sbin/reboot' | sudo EDITOR='tee -a' visudo
